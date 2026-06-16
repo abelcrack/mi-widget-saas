6c997f765177f50d86b064b1316279a089a75a1f
abelcrack <abelccenterojas@gmail.com>
2026-06-15 19:23:44 -0500

    Mostrar index.html en la ruta principal

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Exportar la app para Vercel
 module.exports = app;
